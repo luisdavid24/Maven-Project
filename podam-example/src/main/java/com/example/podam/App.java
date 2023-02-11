678b479179ad793fef09288afb8af8b622817b13
Luis david <david12.29@hotmail.com>
2023-02-11 15:43:15 -0500

    Begin

---
+++ podam-example/src/main/java/com/example/podam/App.java
@@ -0,0 +1,13 @@
+package com.example.podam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
